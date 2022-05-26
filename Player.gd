0fd2586d77c377959f47c25a9e4e115de5d11fb6
Oscar Maia <oscarfgmaia@gmail.com>
2022-05-26 08:17:13 -0300

    Dodge game tutorial completed

---
+++ Player.gd
@@ -0,0 +1,55 @@
+extends Area2D
+signal hit
+
+export var speed = 400
+var screen_size
+#de enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport().size
+	hide()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+
+	
+	if velocity.length() > 0:
+		velocity=velocity.normalized()*speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x<0
+	
+	if velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.flip_v = velocity.y>0
+	
+	position += velocity * delta
+	position.x = clamp(position.x,0,screen_size.x)
+	position.y = clamp(position.y,0,screen_size.y)
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled",true)
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
